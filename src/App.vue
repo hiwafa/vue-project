8724193483a7370b63f06424332eb8a013d2b9cf
hiwafa <mq.func@gmail.com>
2024-04-29 12:04:01 +0200

    from zero

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,14 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <header>
+  <div class="wrapper">
+    
+    This is the simplest vue page
 
-    <div>
-      <HelloWorld msg="You did it!" />
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  </div>
 </template>
 
 <style scoped>
--- src/views/AboutView.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div >
-    <h1>About page</h1>
-  </div>
-</template>
-
-<style>
-
-</style>
