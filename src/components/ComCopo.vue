073fc68a1172ac21e3d3b462488e04a0a00a59fd
hiwafa <mq.func@gmail.com>
2024-05-04 00:03:59 +0200

    an important comment

--- src/components/ComCopo.vue
+++ src/components/ComCopo.vue
@@ -1,11 +1,37 @@
 <script setup>
-    import {ref} from "vue";
+    import {ref, reactive} from "vue";
 
     const message = ref("Com Hello World");
+    const counter = reactive({
+        count: 110
+    });
+
+
+
+    /*
     
+
+    // options api
+    export default {
+        name: "ComponentName",
+        data(){
+            return {
+                var1: "value 1"
+            }
+        }
+    }
+
+
+    composition api
+    import {ref} from "vue";
+    const var1 = ref("value 1");
+    
+    */
+
 </script>
 
 <template>
 
    {{message}}
+   {{counter.count}}
 </template>
