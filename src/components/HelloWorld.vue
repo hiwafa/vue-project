c08fef460875c8fefe236b22d88f2b52c9946bed
hiwafa <mq.func@gmail.com>
2024-05-13 15:38:05 +0200

    a bit more on watch and computed

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,11 +2,46 @@
 
 import ComCopo from "./ComCopo.vue"
 
-export default{
+export default {
     name: "HelloWorld",
     props: ['msg', "heroName"],
     components: {
         ComCopo
+    },
+
+    data() {
+        return {
+            myName: "Wafa"
+        }
+    },
+    methods: {
+        displayData() {
+
+            this.setter = "New Set Value 9999";
+            return this.setter
+        }
+    },
+    computed: {
+        computedFunc() {
+            return "This is a computed function"
+        },
+        setter: {
+            get() {
+                return "This is the data returned from a method"
+            },
+            set(val) {
+                console.log("New Value: ", val)
+            }
+        }
+    },
+    watch: {
+        myName: {
+            handler(newVal) {
+                console.log("My Name is has changed:", newVal)
+            },
+            immediate: true,
+            deep: true
+        }
     }
 }
 
@@ -15,6 +50,11 @@ export default{
 <template>
 
     <ComCopo />
-    <div> This is Hello world Component: {{msg}} - {{heroName}}</div>
-    
+    <div> This is Hello world Component: {{ msg }} - {{ heroName }}</div>
+    <div>{{ displayData() }}</div>
+    <div>{{ computedFunc }}</div>
+    <input type="text" v-model="myName">
+    <p>{{ myName }}</p>
+    <p>{{ setter }}</p>
+
 </template>
