167b05496862b808158d2161c66747e71cdc0efd
hiwafa <mq.func@gmail.com>
2024-05-14 16:17:45 +0200

    text binding two way

--- src/components/AdvanceVue.vue
+++ src/components/AdvanceVue.vue
@@ -4,10 +4,27 @@
 
         <button @click="childClick('From Child')">Click Parent Function</button>
 
+        <input :value="textValue" @input="hanldeTextBinding" placeholder="Type Here.." />
+        <input v-model="textModel" placeholder="Type your text.." />
+        <p>{{ textValue }}</p>
+        <p>{{ textModel }}</p>
+
+
     </div>
 </template>
 
 <script setup>
+
+import { ref } from 'vue';
+
+const textValue = ref("");
+const textModel = ref("");
+
+const hanldeTextBinding = event => {
+    textValue.value = event.target.value;
+}
+
+
 
 const { topic, childClick } = defineProps({
     topic: String,
